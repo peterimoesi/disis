2e808e05cd0eeb45e7e209fe98ee3a09a7370830
Peter Imoesi <peterimoesi@yahoo.com>
2018-08-01 19:15:24 +0300

    landing page

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,55 +1,60 @@
 import React from 'react';
-import {
-    Navbar,
-    NavbarBrand,
-    NavbarNav,
-    NavItem,
-    NavLink
-    // NavbarToggler,
-    // Collapse,
-    // NavLinkContainer
-} from 'mdbreact';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-
+import history from '../../routes/history';
 import { removeUserCredentials } from '../../actions/setUserCredentials';
 import './styles.scss';
 
 class NavbarComponent extends React.Component {
     constructor() {
         super();
-        this.state = {};
+        this.state = {
+            open : false
+        };
+        this.toggleNav = this.toggleNav.bind(this);
     }
+
+    toggleNav() {
+        this.setState({ isOpen : !this.state.isOpen });
+    }
+
     render() {
         return (
-            <Navbar color="transparent" dark expand="lg" fixed="top" className="top-nav-bar" scrolling>
+            <Navbar dark expand="md" fixed="top" className="top-nav-bar">
                 <div className="container">
                     <NavbarBrand href="#">
                         DISIS
                     </NavbarBrand>
-                    {
-                        this.props.isAuthenticated ?
-                            <NavbarNav right>
-                                <NavItem>
-                                    <NavLink to="/app/dashboard">My page</NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink
-                                        to="/app/login"
-                                        onClick={e => { e.preventDefault(); this.props.removeUserCredentials(); }}
-                                    >Logout</NavLink>
-                                </NavItem>
-                            </NavbarNav> :
-                            <NavbarNav right>
-                                <NavItem>
-                                    <NavLink to="/app/login">Login</NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink to="/app/signup">Signup</NavLink>
-                                </NavItem>
-                            </NavbarNav>
-                    }
-                    
+                    <NavbarToggler onClick={this.toggleNav} className="mr-2" />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        {
+                            this.props.isAuthenticated ?
+                                <Nav right="true" navbar>
+                                    <NavItem>
+                                        <NavLink
+                                            to="/app/dashboard"
+                                            onClick={e => { e.preventDefault(); history.push('/app/dashboard'); }}
+                                        >
+                                            My page</NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink
+                                            to="/app/login"
+                                            onClick={e => { e.preventDefault(); this.props.removeUserCredentials(); }}
+                                        >Logout</NavLink>
+                                    </NavItem>
+                                </Nav> :
+                                <Nav right="true" navbar>
+                                    <NavItem>
+                                        <NavLink to="/app/login" onClick={e => { e.preventDefault(); history.push('/app/login'); }}>Login</NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink to="/app/signup"  onClick={e => { e.preventDefault(); history.push('/app/signup'); }}>Signup</NavLink>
+                                    </NavItem>
+                                </Nav>
+                        }
+                    </Collapse>
                 </div>
             </Navbar>
         );
--- src/screens/landing/index.jsx
+++ src/screens/landing/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 // import { HashLink } from 'react-router-hash-link';
+import { Button } from 'mdbreact';
 
 import Navbar from '../../components/nav';
 import landingVideo from '../../public/videos/landing.mp4';
@@ -23,15 +24,41 @@ class Landing extends React.Component {
                     <div className="bg-vid-overlay">
                         <div className="container">
                             <div className="row">
-                                <div className="col-lg-12">
-                                    <div>
-                                        <button type="button" className="btn btn-default btn-sm button-no-border">
-                                            <span className="glyphicon glyphicon-menu-down" />
-                                        </button>
+                                <div className="col-lg-6 col-md-6 col-sm-24 col-xs-24">
+                                    <h1 className="landing-main txt-center">DISIS ME</h1>
+                                    <div className="txt-center">
+                                        <Button color="dark-green">
+                                            Click to begin
+                                        </Button>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6 col-md-6 hiddden-sm hidden-xs">
+                                    <div className="list-cont">
+                                        <ul>
+                                            <li>Host your Resume to your customizable page in seconds</li>
+                                            <li>Use and share your own custom link with future employer</li>
+                                            <li>Select one of the many custom themes</li>
+                                            <li>Make the theme your own from 16 million color combinations</li>
+                                            <li>Update your information in a matter of seconds</li>
+                                            <li>Avoid the hassle of downloading, editing and uploading PDFs</li>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                    </div>
+                </div>
+                <div className="hidden-lg hidden-md external-list list-cont">
+                    <h3 className="txt-center">About</h3>
+                    <div>
+                        <ul>
+                            <li>Host your Resume to your customizable page in seconds</li>
+                            <li>Use and share your own custom link with future employer</li>
+                            <li>Select one of the many custom themes</li>
+                            <li>Make the theme your own from 16 million color combinations</li>
+                            <li>Update your information in a matter of seconds</li>
+                            <li>Avoid the hassle of downloading, editing and uploading PDFs</li>
+                        </ul>
                     </div>
                 </div>
                 <div className="container">
